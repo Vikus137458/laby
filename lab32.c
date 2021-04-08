c5e8e25db4d8c91484d067808ffd64e191063535
user <you@example.com>
2021-04-08 12:52:54 +0500

    lab3

---
+++ lab32.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    double x0, h, n, x, f;
+    int i;
+    printf("Vvedite shag h");
+    scanf ("%lf", &h);
+    printf("x\t\t f(x)\n");
+    printf("------------------------\n");
+    x0 = 0;
+    n = 1.6 / h;
+    i = 0;
+    for (i = 0; i <= n; i++)
+    {
+        x = x0 + i * h;
+        if (x <= 0.6)
+            f = 1 / (1 + 25 * x * x);
+        else
+            f = (x + 2 * x * x * x * x) * sin (x * x);
+        printf("%lf\t\t %5.4lf\n", x, f);
+    }
+    return 0;
+}
