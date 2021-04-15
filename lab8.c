63971f049b796da69cce820b71ad759a0833a39d
user <user@ws1013402143.pm>
2021-04-15 16:47:20 +0500

    lab8

---
+++ lab8.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <termios.h>
+#include <unistd.h>
+#include <ctype.h>
+
+int mygetch()
+{
+    struct termios oldt, newt;
+    int c;
+    tcgetattr(STDIN_FILENO, &oldt);
+    newt = oldt;
+    newt.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+    c = getchar();
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    return c;
+}
+
+int main()
+{
+    char a;
+    int i = 1;
+    //while (i != 0)
+    //{
+    printf("%hhd", a);
+    if (isalnum(a) != 0)
+    {
+        a = mygetch();
+        scanf("%hhd", &a);
+    }
+    else
+        scanf("%hhd", &a);
+    return 0;
+}
