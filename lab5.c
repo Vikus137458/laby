d9f34219bdb768964091e77ea71a1b6119969705
user <user@ws1013402143.pm>
2021-04-15 15:28:53 +0500

    lab5

--- lab5.c
+++ lab5.c
@@ -2,9 +2,15 @@
 #include <math.h>
 int f1(int n)
 {
-    if (n / 10 != 0)
-    return n ? n % 10 f1(n / 10) : 1;
+    if (n >= 10)
+        printf("%d", n % 10);
+    else
+        printf("%d", n);
+    n = n / 10;
+    if (n >= 10)
+        f1(n);
 }
+
 int f2(int n)
 {
     int i = 0;
@@ -15,10 +21,12 @@ int f2(int n)
     }
     return i;
 }
-int main(){
+
+int main()
+{
     int n;
-    scanf ("%d", &n) ;
-    printf ("Recursion %d\n", f1(n));
-    printf ("Cycle %d", f2(n)) ;
+    scanf("%d", &n);
+    printf("%d\n", f1(n));
+    printf("%d", f2(n));
     return 0;
 }
