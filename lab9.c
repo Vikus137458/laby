54858c08cb14760927830adb53fd6d12da8f8239
Vikus137458 <80880904+Vikus137458@users.noreply.github.com>
2021-04-22 10:18:22 +0500

    Update lab9.c

--- lab9.c
+++ lab9.c
@@ -1,12 +1,30 @@
-# include < stdio .h >
-# include < string .h >
+# include <stdio.h>
+# include <string.h>
+
 int main ()
 {
-    char str [41] = "I ’ m going off the rails on a crazy train ";
-    char delim [3] = " , ";
-    char * p = strtok ( str , delim ) ;
-    printf ( p ) ;
-    while ( p = strtok ( NULL , delim ) )
-    printf ("|% s " , p ) ;
+    char s[10];
+    scanf("%s", s);
+    char A[10];
+    char s3[1] = " ";
+    char s1[10] = "";
+      int i = k = 0;
+      int j;
+     while (i < strlen(s))
+     {
+         
+         while (s[i] != s3)
+         {
+           strcat (s1, s[i]);
+           i++;
+         }
+        i++;
+        A[k] = s1;
+        k++;
+        for (j = 0;j < 10;j++)
+            if A[j] != A[k]
+                printf ("%c", A[k]);   
+     }
     return 0;
 }
+
