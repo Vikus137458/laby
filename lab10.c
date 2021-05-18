823044af41c3d66df9ef635a236f4606f949592d
Vikus137458 <80880904+Vikus137458@users.noreply.github.com>
2021-05-18 16:12:51 +0500

    Update lab10.c

--- lab10.c
+++ lab10.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-void f1(int n, int m, int a[n][m])
+void f1(int n, int m, int **a)
 {
     int i, j;
     for (i = 0; i < n; i++)
         for (j = 0; j < m; j++)
             a[i][j] = rand() % 21 - 10;
 }
-void f2(int n, int m, int a[n][m])
+void f2(int n, int m, int **a)
 {
     int i, j;
     for (i = 0; i < n; i++)
@@ -25,21 +25,23 @@ int main()
     int n, m, x, i, j, k;
     printf("Vvedite n and m\n");
     scanf("%d\t%d", &n, &m);
+    
     int **a = (int **)malloc(n * sizeof(int *));
-    if (!a)
+    if (NULL == a)
     {
-        printf("Memory allocation error !\ n");
+        printf(" Memory allocation error !\n ");
         exit(EXIT_FAILURE);
     }
     for (i = 0; i < n; i++)
-        p[i] = malloc(m * sizeof(int));
-    for (i = 0; i < n; i++)
-        for (j = 0; j < m; j++)
-            a[i][j] = i * m + j + 1;
-    for (i = 0; i < n; i++)
-        free(a[i]);
-    free(a);
-    int a[n][m];
+    {
+        a[i] = (int *)malloc(m * sizeof(int));
+        if (NULL == a)
+        {
+            printf(" Memory allocation error !\n ");
+            exit(EXIT_FAILURE);
+        }
+    }
+    
     f1(n, m, a);
     f2(n, m, a);
     for (k = 0; k < n; k++)
@@ -52,5 +54,9 @@ int main()
                     a[i + 1][j] = x;
                 }
     f2(n, m, a);
+    
+    for (i = 0; i < n; i++)
+        free(a[i]);
+    free(a);
     return 0;
 }
